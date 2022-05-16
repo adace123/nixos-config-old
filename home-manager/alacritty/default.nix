5b82fb3df121c2bd1a2a6ed4efcd1f17519c553e
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-16 00:18:58 -0700

    move alacritty config to nix file

--- home-manager/alacritty/default.nix
+++ home-manager/alacritty/default.nix
@@ -1,8 +1,33 @@
-{ pkgs, ... }:
+{ alacrittyTheme ? "catpuccin", pkgs, ... }:
+let 
+   themes = (import ./themes.nix {});
+in
 {
-  home.packages = [
-    pkgs.alacritty
-  ];
 
-  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env = { TERM = "xterm-256color"; };
+      window = {
+        opacity = 0.8;
+	dynamic_padding = true;
+	dynamic_title = true;
+	padding = {
+          x = 6;
+	  y = 6;
+	};
+      };
+
+      mouse = { hide_when_typing = true; };
+
+      font = let fontname = "Hack Nerd Font Mono"; in {
+	normal = { family = fontname; };
+	bold = { family = fontname; style = "Bold"; };
+        italic = { family = fontname; style = "Semibold Italic"; };
+	size = 11;
+      };
+
+      colors = themes.${alacrittyTheme};
+    };
+  };
 }
