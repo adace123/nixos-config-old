761f128648af0d804b49ff8902075925fe2299f0
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-19 23:04:40 -0700

    grab themes from alacritty-theme repo

--- home-manager/alacritty/default.nix
+++ home-manager/alacritty/default.nix
@@ -1,38 +1,16 @@
 { pkgs, ... }:
 let 
-   themes = (import ./themes.nix {});
-   repo = pkgs.fetchFromGitHub {
+   alacritty-themes = pkgs.fetchFromGitHub {
     owner = "eendroroy";
     repo = "alacritty-theme";
     rev = "ade1c91";
     sha256 = "19bprgkmy08g58akijzwbfrmfipxzcdkvnadk5k94jklbjayiiff";
    };
-
-   alacritty-colorscheme = pkgs.python310Packages.buildPythonPackage rec {
-     pname = "alacritty-colorscheme";
-     version = "1.0.1";
-
-     src = pkgs.fetchFromGitHub {
-       owner = "toggle-corp";
-       repo = "alacritty-colorscheme";
-       rev = "4dd944c";
-       sha256 = "0g6kpnv9q6vspjwrl661bzzfpnxzw2138ll5gldjvb17pw6rg5rp";
-     };
-     #src = pkgs.python310Packages.fetchPypi {
-     #  inherit pname version;
-     #  sha256 = "779fa9b7c2352050ca50758c74493a7599ede8a584a089ad9fc7bbc0d9b51c53";
-     #};
-     
-     doCheck = false;
-   };
+   
+   selectedTheme = "gruvbox_dark";
+   theme = (import ../../utils.nix {}).fromYAML "${alacritty-themes}/themes/${selectedTheme}.yaml";
 in
 {
-  home.file.".config/alacritty/colors".source = "${repo}/themes";
-
-  home.packages = with pkgs; [
-    alacritty-colorscheme
-  ];
-
   programs.alacritty = {
     enable = true;
     settings = {
@@ -56,7 +34,6 @@ in
 	size = 11;
       };
 
-      colors = themes.gruvbox_dark;
-    };
+    } // theme;
   };
 }
---
+++ utils.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+{
+  fromYAML = path:
+    let
+      fileContents = builtins.readFile path;
+      fileToJSON = pkgs.stdenv.mkDerivation {
+        name = "fromYAML";
+	phases = [ "buildPhase" ];
+	buildPhase = "${pkgs.yaml2json}/bin/yaml2json < ${builtins.toFile "yaml" fileContents} > $out";
+      };
+    in
+      builtins.fromJSON (builtins.readFile fileToJSON);
+}
