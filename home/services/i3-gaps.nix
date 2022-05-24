5b2c3d9831ba04fed39241c951f848e6d20d4a03
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-24 00:52:16 -0700

    update i3 config

--- home/services/i3-gaps.nix
+++ home/services/i3-gaps.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
-let
-  mod = "Mod4";
-in
 {
   xsession.windowManager.i3 = {
     enable = true;
     
     package = pkgs.i3-gaps;
+
+    options = {
+      modifier = "Mod4";
+    };
     
     config = {
       keybindings = {
         "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";  
+        "${mod}+q" = "kill";
       };
     };
   };
