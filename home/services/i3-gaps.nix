7805bbbcf40d4f72993d14cf6c909914daeee1c3
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-24 01:26:34 -0700

    fix undefined var

--- home/services/i3-gaps.nix
+++ home/services/i3-gaps.nix
@@ -4,8 +4,8 @@
 
     package = pkgs.i3-gaps;
 
-    config = {
-      modifier = "Mod4";
+    config = rec {
+      mod = "Mod4";
       keybindings = {
         "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
         "${mod}+q" = "kill";
