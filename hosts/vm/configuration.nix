c788fe8b6c949bf8f9850411c99d4aed24b9acf4
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-25 00:21:32 -0700

    run sshd, mount nixos-config dir

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -11,6 +11,19 @@
     memorySize = 4096; # MB
     writableStoreUseTmpfs = false;
     graphics = true;
+
+    sharedDirectories = {
+      nixos-config = {
+        source = "/home/aaron/nixos-config";
+        target = "/mnt/nixos-config";
+      };
+    };
+
+    forwardPorts = [{
+      from = "host";
+      host.port = 2222;
+      guest.port = 22;
+    }];
   };
 
   # use unstable nix so we can access flakes
@@ -73,12 +86,19 @@
     hashedPassword =
       "$5$/nhm9p0UG6PMMe5t$0P/i5UAlQItu16yjr8h/Xw/qKDqp9nfHlys3sze0dmC";
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDY/cDE4jLKzlng2ihxAW9Xnv4YrrUnNeaBdNtWqpHfVIZyveCr7NIFZ9bsgKYRrBFoQhs8fNNPdexXqXxLwiPe+JO3BGWMNH71fB8hixIC+c6CcYNVpEQxKdW3EJONeRGovYlIVsgNf4DgUgnoEwBSh6N0yuEHkHK64kYt6VTkLHFW+Gryefuuuzv9roMJ3vYtWxd40rkVPIOVqnGC4n7nDrDXvy3A9Dy7RHVKfgASrI83xJvm9x+CEnzmLi+Gco7HXo8h+fmYc3XqTlsfZVAielzJuaiDTZEbkL9Hi/HsWj9YkxS7gj/+fETigMHgP8vDwsFcLMESh1kvsgQXU9HyFD2cX2mezRKTaVjSOiNPM+oVJu7pwu0uwgFZ/pKo6G/B3+5jQ3cRazXeNnhtAF9n0QvusKW17I/LepS8IvxeHBdDx9RZB4eRZQt9bto0QqnoKqJD1ivduH8gSDqPRXCC5ji1xSkbby5hnkBYQNF87NBI++AjrzdgynMvv4MO4bM= aaron@aaron-endeavouros"
+
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
   };
 
   users.extraUsers.root.password = "";
 
-  # Manage fonts. We pull these from a secret directory since most of these
-  # fonts require a purchase.
   fonts = {
     fontDir.enable = true;
     fonts = with pkgs; [
@@ -91,5 +111,5 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [ git home-manager ];
+  environment.systemPackages = with pkgs; [ git home-manager vim ];
 }
