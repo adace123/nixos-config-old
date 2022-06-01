acfc8f50f493007fe7748a3d5513bc236f630401
Aaron Feigenbaum <rexforddrive@gmail.com>
2022-05-31 21:09:18 -0700

    update i3 config

--- home/services/i3-gaps.nix
+++ home/services/i3-gaps.nix
@@ -11,6 +11,7 @@ in {
         "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
         "${mod}+q" = "kill";
 
+        # workspace navigation
         "${mod}+1" = "workspace number 1";
         "${mod}+2" = "workspace number 2";
         "${mod}+3" = "workspace number 3";
@@ -21,7 +22,9 @@ in {
         "${mod}+8" = "workspace number 8";
         "${mod}+9" = "workspace number 9";
         "${mod}+0" = "workspace number 10";
+        "${mod}+Tab" = "workspace back_and_forth";
 
+        # move containers to workspaces
         "${mod}+Shift+1" = "move container to workspace number 1";
         "${mod}+Shift+2" = "move container to workspace number 2";
         "${mod}+Shift+3" = "move container to workspace number 3";
@@ -48,7 +51,63 @@ in {
 
         "${mod}+f" = "fullscreen toggle";
         "${mod}+Shift+space" = "floating toggle";
+
+        "${mod}+minus" =
+          "floating toggle; resize set 1000 600, move scratchpad";
+        "${mod}+plus" = "scratchpad show; move position center";
+
+        "${mod}+r" = ''mode "resize"'';
+        "${mod}+Shift+Delete" = ''mode "system"'';
+
+        "${mod}+XF86AudioPlay" =
+          "exec ${pkgs.playerctl}/bin/playerctl play-pause";
       };
+
+      modes = {
+        resize = {
+          "l" = "resize shrink width 10 px or 10 ppt";
+          "j" = "resize grow height 10 px or 10 ppt";
+          "k" = "resize shrink height 10 px or 10 ppt";
+          "h" = "resize grow width 10 px or 10 ppt";
+          "Escape" = ''mode "default"'';
+          "Return" = ''mode "default"'';
+        };
+
+        system = {
+          "r" = "exec --no-startup-id reboot";
+          "s" = "exec --no-startup-id shutdown now";
+          "Escape" = ''mode "default"'';
+          "Return" = ''mode "default"'';
+        };
+      };
+
+      startup = [
+        {
+          command = "pgrep ${pkgs.brave}/bin/brave || ${pkgs.brave}/bin/brave";
+          always = true;
+          notification = false;
+        }
+        {
+          command = "alacritty --class=alacritty-init";
+          always = false;
+          notification = false;
+        }
+      ];
+
+      gaps = {
+        smartBorders = "on";
+        smartGaps = true;
+        inner = 6;
+        outer = 3;
+      };
+
+      window = {
+        commands = [{
+          command = "floating enable, resize set 1000 600, move to scratchpad";
+          criteria = { instance = "alacritty"; };
+        }];
+      };
+
     };
   };
 }
